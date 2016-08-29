db3a5a2272cfdf0c049ce342c81e03d9e86a4f8c
Nepxion <1394997@qq.com>
2016-08-29 17:26:46 +0800

    Spring Boot和Docker集成

--- nepxion-thunder-spring-boot-docker/install.bat
+++ nepxion-thunder-spring-boot-docker/install.bat
@@ -2,6 +2,4 @@ set DOCKER_HOST=tcp://192.168.99.100:2376
 
 set DOCKER_CERT_PATH=C:\Users\Neptune\.docker\machine\certs
 
-call mvn package && java -jar target/nepxion-thunder-spring-boot-docker-1.0.0.jar
-
-call mvn clean package docker:build
+call mvn clean package docker:build  && java -jar target/nepxion-thunder-spring-boot-docker-1.0.0.jar
