86cb4a766cdfaedec9eca575c4f231aafdd9dc45
Nepxion <1394997@qq.com>
2017-03-20 21:40:43 +0800

    增加Test

---
+++ nepxion-thunder-stock/src/test/java/com/nepxion/thunder/stock/StockTest.java
@@ -0,0 +1,15 @@
+package com.nepxion.thunder.stock;
+
+/**
+ * <p>Title: Nepxion Stock</p>
+ * <p>Description: Nepxion Stock For Distribution</p>
+ * <p>Copyright: Copyright (c) 2015</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+public class StockTest {
+
+}
