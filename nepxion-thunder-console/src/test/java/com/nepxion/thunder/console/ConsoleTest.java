bc12446d87d89274fdf2897a81e5c5dcc599f00f
Nepxion <1394997@qq.com>
2017-03-20 21:33:28 +0800

    增加ConsoleTest

---
+++ nepxion-thunder-console/src/test/java/com/nepxion/thunder/console/ConsoleTest.java
@@ -0,0 +1,15 @@
+package com.nepxion.thunder.console;
+
+/**
+ * <p>Title: Nepxion Thunder</p>
+ * <p>Description: Nepxion Thunder For Distribution</p>
+ * <p>Copyright: Copyright (c) 2015</p>
+ * <p>Company: Nepxion</p>
+ * @author Neptune
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+public class ConsoleTest {
+
+}
