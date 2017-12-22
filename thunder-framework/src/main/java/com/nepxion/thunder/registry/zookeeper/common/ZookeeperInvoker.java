70aba4f55606411d6d4f39ceef0a9c7d4ee8089a
Nepxion <1394997@qq.com>
2017-12-22 11:44:23 +0800

    增加Zookeeper状态判断

--- thunder-framework/src/main/java/com/nepxion/thunder/registry/zookeeper/common/ZookeeperInvoker.java
+++ thunder-framework/src/main/java/com/nepxion/thunder/registry/zookeeper/common/ZookeeperInvoker.java
@@ -19,7 +19,12 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.api.ExistsBuilder;
+import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.retry.BoundedExponentialBackoffRetry;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.retry.RetryForever;
 import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.retry.RetryUntilElapsed;
 import org.apache.curator.utils.PathUtils;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
@@ -28,11 +33,45 @@ import com.nepxion.thunder.serialization.SerializerException;
 import com.nepxion.thunder.serialization.SerializerExecutor;
 
 public class ZookeeperInvoker {
+    // 重试指定的次数, 且每一次重试之间停顿的时间逐渐增加
+    public RetryPolicy createExponentialBackoffRetry(int baseSleepTimeMs, int maxRetries) {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(baseSleepTimeMs, maxRetries);
+
+        return retryPolicy;
+    }
+
+    // 重试指定的次数, 且每一次重试之间停顿的时间逐渐增加，增加了最大重试次数的控制
+    public RetryPolicy createBoundedExponentialBackoffRetry(int baseSleepTimeMs, int maxSleepTimeMs, int maxRetries) {
+        RetryPolicy retryPolicy = new BoundedExponentialBackoffRetry(baseSleepTimeMs, maxSleepTimeMs, maxRetries);
+
+        return retryPolicy;
+    }
+
+    // 指定最大重试次数的重试
+    public RetryPolicy createRetryNTimes(int count, int sleepMsBetweenRetries) {
+        RetryPolicy retryPolicy = new RetryNTimes(count, sleepMsBetweenRetries);
+
+        return retryPolicy;
+    }
+
+    // 永远重试
+    public RetryPolicy createRetryForever(int retryIntervalMs) {
+        RetryPolicy retryPolicy = new RetryForever(retryIntervalMs);
+
+        return retryPolicy;
+    }
+
+    // 一直重试，直到达到规定的时间 
+    public RetryPolicy createRetryUntilElapsed(int maxElapsedTimeMs, int sleepMsBetweenRetries) {
+        RetryPolicy retryPolicy = new RetryUntilElapsed(maxElapsedTimeMs, sleepMsBetweenRetries);
+
+        return retryPolicy;
+    }
 
     // 创建ZooKeeper客户端实例
     public CuratorFramework create(String address, int sessionTimeout, int connectTimeout, int connectWaitTime) {
-        // RetryPolicy retryPolicy = new ExponentialBackoffRetry(connectWaitTime, 29); // 连一次连不上，下次连接时间递增
-        RetryPolicy retryPolicy = new RetryNTimes(Integer.MAX_VALUE, connectWaitTime); // 不断的频繁重试
+        // RetryPolicy retryPolicy = createExponentialBackoffRetry(connectWaitTime, 29);
+        RetryPolicy retryPolicy = createRetryNTimes(Integer.MAX_VALUE, connectWaitTime);
         CuratorFramework client = CuratorFrameworkFactory.newClient(address, sessionTimeout, connectTimeout, retryPolicy);
 
         return client;
@@ -60,6 +99,22 @@ public class ZookeeperInvoker {
         client.close();
     }
 
+    // 获取ZooKeeper客户端连接是否正常
+    public static boolean isStarted(CuratorFramework client) {
+        return client.getState() == CuratorFrameworkState.STARTED;
+    }
+
+    // 检查ZooKeeper启动状态
+    public static void validateStatus(CuratorFramework client) throws Exception {
+        if (client == null) {
+            throw new ZookeeperException("Zookeeper isn't initialized");
+        }
+
+        if (!isStarted(client)) {
+            throw new ZookeeperException("Zookeeper isn't started");
+        }
+    }
+
     // 判断路径是否存在
     public boolean pathExist(CuratorFramework client, String path) throws Exception {
         return getPathStat(client, path) != null;
@@ -67,6 +122,7 @@ public class ZookeeperInvoker {
 
     // 判断stat是否存在
     public Stat getPathStat(CuratorFramework client, String path) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         ExistsBuilder builder = client.checkExists();
@@ -81,6 +137,7 @@ public class ZookeeperInvoker {
 
     // 创建路径
     public void createPath(CuratorFramework client, String path) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         client.create().creatingParentsIfNeeded().forPath(path, null);
@@ -88,6 +145,7 @@ public class ZookeeperInvoker {
 
     // 创建路径，并写入数据
     public void createPath(CuratorFramework client, String path, byte[] data) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         client.create().creatingParentsIfNeeded().forPath(path, data);
@@ -95,6 +153,7 @@ public class ZookeeperInvoker {
 
     // 创建路径，并写入对象
     public void createPath(CuratorFramework client, String path, Serializable object) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         byte[] data = getData(object);
@@ -104,6 +163,7 @@ public class ZookeeperInvoker {
 
     // 创建路径
     public void createPath(CuratorFramework client, String path, CreateMode mode) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         client.create().creatingParentsIfNeeded().withMode(mode).forPath(path, null);
@@ -111,6 +171,7 @@ public class ZookeeperInvoker {
 
     // 创建路径，并写入数据
     public void createPath(CuratorFramework client, String path, byte[] data, CreateMode mode) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         client.create().creatingParentsIfNeeded().withMode(mode).forPath(path, data);
@@ -118,6 +179,7 @@ public class ZookeeperInvoker {
 
     // 创建路径，并写入对象
     public void createPath(CuratorFramework client, String path, Serializable object, CreateMode mode) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         byte[] data = getData(object);
@@ -127,6 +189,7 @@ public class ZookeeperInvoker {
 
     // 删除路径
     public void deletePath(CuratorFramework client, String path) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         client.delete().deletingChildrenIfNeeded().forPath(path);
@@ -134,6 +197,7 @@ public class ZookeeperInvoker {
 
     // 获取数据
     public byte[] getData(CuratorFramework client, String path) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         return client.getData().forPath(path);
@@ -141,6 +205,7 @@ public class ZookeeperInvoker {
 
     // 获取对象
     public <T> T getObject(CuratorFramework client, String path, Class<T> clazz) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         byte[] data = client.getData().forPath(path);
@@ -150,6 +215,7 @@ public class ZookeeperInvoker {
 
     // 写入数据
     public void setData(CuratorFramework client, String path, byte[] data) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         client.setData().forPath(path, data);
@@ -157,6 +223,7 @@ public class ZookeeperInvoker {
 
     // 写入对象
     public void setData(CuratorFramework client, String path, Serializable object) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         byte[] data = getData(object);
@@ -184,6 +251,7 @@ public class ZookeeperInvoker {
 
     // 获取子节点名称列表
     public List<String> getChildNameList(CuratorFramework client, String path) throws Exception {
+        validateStatus(client);
         PathUtils.validatePath(path);
 
         return client.getChildren().forPath(path);
@@ -201,29 +269,4 @@ public class ZookeeperInvoker {
 
         return childPathList;
     }
-
-    // ZooKeeper节点的路径必须存在，如果不存在则立即创建
-    /*public boolean createPath(final CuratorFramework client, final String path) throws Exception {
-        PathUtils.validatePath(path);
-
-        final CuratorZookeeperClient client = client.getZookeeperClient();*/
-
-        /*
-         * 操作重试，如果在执行一个操作时，遇到了ZooKeeper链接异常：
-         * 1. RetryLoop可以不断重试，直到网络正常且操作执行成功为止
-         * 2. RetryLoop只负责KeeperException，不负责其他RuntimeException，
-         *    一旦KeeperException被捕获，就开始Retry
-         *    一旦其他异常被捕获，就停止Retry
-         *    无任何异常，不Retry 
-         */
-        /*return RetryLoop.callWithRetry(client, new Callable<Boolean>() {
-            @Override
-            public Boolean call() throws Exception {
-                EnsurePath ensure = new EnsurePath(path);
-                ensure.ensure(client);
-
-                return true;
-            }
-        });
-    }*/
 }
