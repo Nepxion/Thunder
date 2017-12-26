e4293249cbae39708d924d069ee0b6bca6d02431
Nepxion <1394997@qq.com>
2017-12-26 11:37:45 +0800

    重构Bat

--- install-docker.bat
+++ install-docker.bat
@@ -19,33 +19,34 @@
 @set JAVA_HOME=E:\Tool\JDK-1.7.0
 @echo Found JAVA_HOME=%JAVA_HOME%
 
+@set DEPENDENCY_LIST=thunder-framework,thunder-test
 @set PROJECT_NAME=thunder-spring-boot-docker-example
 @set IMAGE_NAME=thunder-spring-boot
+@set MACHINE_PORT=6010
+@set CONTAINER_PORT=6010
 
-@rem 删除target，有时候mvn会clean失败，需事先强制删除target
 if exist %PROJECT_NAME%\target rmdir /s/q %PROJECT_NAME%\target
 
-@rem 执行相关模块的install
-call mvn clean install -DskipTests -pl thunder-framework,thunder-test,%PROJECT_NAME% -am
+@rem 执行相关模块的Maven Install
+call mvn clean install -DskipTests -pl %DEPENDENCY_LIST% ,%PROJECT_NAME% -am
 
-@rem 停止和删除容器
+@rem 停止和删除Docker容器
 call docker stop %IMAGE_NAME%
 @rem call docker kill %IMAGE_NAME%
 call docker rm %IMAGE_NAME%
 
-@rem 删除镜像
+@rem 删除Docker镜像
 call docker rmi %IMAGE_NAME%
 
 cd %PROJECT_NAME%
 
-@rem 安装容器镜像
 set DOCKER_HOST=tcp://localhost:2375
-@rem set DOCKER_HOST=tcp://192.168.99.100:2376
 @rem set DOCKER_CERT_PATH=C:\Users\Neptune\.docker\machine\certs
 
+@rem 安装Docker镜像
 call mvn package docker:build -DskipTests
-@rem call mvn package docker:build -DskipTests && java -jar target\%PROJECT_NAME%-1.0.0.jar
 
-call docker run -i -t -p 6010:6010 --name %IMAGE_NAME% %IMAGE_NAME%:latest
+@rem 安装和启动Docker容器，并自动执行端口映射
+call docker run -i -t -p %MACHINE_PORT%:%CONTAINER_PORT% --name %IMAGE_NAME% %IMAGE_NAME%:latest
 
 pause
