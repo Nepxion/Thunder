fdf21f7c525c6d0c106ba004e5151199c1a42277
Nepxion <1394997@qq.com>
2017-12-29 13:35:46 +0800

    修改脚本

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -18,8 +18,8 @@ color=0a
 PROJECT_NAME=thunder-test
 PROJECT_LIST=thunder-framework,${PROJECT_NAME}
 
-if [ ! -d ${PROJECT_NAME}\target];then
-rmdir /s/q ${PROJECT_NAME}\target
+if [ ! -d ${PROJECT_NAME}/target];then
+rmdir /s/q ${PROJECT_NAME}/target
 fi
 
 # 执行相关模块的Maven Install
