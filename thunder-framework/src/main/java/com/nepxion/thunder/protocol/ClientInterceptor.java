415a753ee2f604b719ee434a55aff4d28bd1ea27
Nepxion <1394997@qq.com>
2018-01-12 19:48:06 +0800

    优化JavaDoc

--- thunder-framework/src/main/java/com/nepxion/thunder/protocol/ClientInterceptor.java
+++ thunder-framework/src/main/java/com/nepxion/thunder/protocol/ClientInterceptor.java
@@ -6,7 +6,6 @@ package com.nepxion.thunder.protocol;
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
- * @email 1394997@qq.com
  * @version 1.0
  */
 
