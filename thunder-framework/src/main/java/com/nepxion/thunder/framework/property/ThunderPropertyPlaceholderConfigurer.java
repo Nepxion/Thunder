5b126e19679cc339bad3e904a7640cc9bdd5809d
Nepxion <1394997@qq.com>
2018-01-12 14:16:12 +0800

    修改COPYRIGHT

--- thunder-framework/src/main/java/com/nepxion/thunder/framework/property/ThunderPropertyPlaceholderConfigurer.java
+++ thunder-framework/src/main/java/com/nepxion/thunder/framework/property/ThunderPropertyPlaceholderConfigurer.java
@@ -3,7 +3,7 @@ package com.nepxion.thunder.framework.property;
 /**
  * <p>Title: Nepxion Thunder</p>
  * <p>Description: Nepxion Thunder For Distribution</p>
- * <p>Copyright: Copyright (c) 2017-2020</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
  * @email 1394997@qq.com
